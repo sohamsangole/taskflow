1f0273e944cd3098885b7042f20ca726a6e6ded9
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-20 02:52:46 +0530

    added homepage to sidebar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskflow/components/sidebar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,23 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Row(
+        children: [
+          SideBar(),
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: const Center(
+                child: Text(
+                  "Main Content",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
