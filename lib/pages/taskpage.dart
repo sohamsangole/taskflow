087b61e419171f617407beb748baefaac4aad8e3
Soham Sangole <sohamajaysangole@gmail.com>
2024-11-20 03:03:58 +0530

    created taskpage

---
+++ lib/pages/taskpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatefulWidget {
+  const TaskPage({super.key});
+
+  @override
+  State<TaskPage> createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
